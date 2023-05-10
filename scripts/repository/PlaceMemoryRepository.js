35c25e1e9dc2b27ee68152e5881f01e3ebe63140
dlgurwls0529 <dlgurwls0529@gmail.com>
2023-05-10 20:29:41 +0900

    replace repository to static repository

--- scripts/repository/PlaceMemoryRepository.js
+++ scripts/repository/PlaceMemoryRepository.js
@@ -6,42 +6,40 @@
 */
 
 class PlaceMemoryRepository {
-    constructor() {
-      this.placeData = [];
-      this.lastId = 0;
-    }
+    static placeData = [];
+    static lastId = 0;
   
     save(item) {
       item.id = ++this.lastId;
-      this.data.push({...item});
+      this.placeData.push({...item});
       return {...item};
     }
   
     update(item) {
-      const index = this.data.findIndex(i => i.id === item.id);
+      const index = this.placeData.findIndex(i => i.id === item.id);
       if (index < 0) {
         throw new Error(`Item with id ${item.id} not found`);
       }
-      this.data[index] = item;
+      this.placeData[index] = item;
       return item;
     }
   
     delete(id) {
-      const index = this.data.findIndex(item => item.id === id);
+      const index = this.placeData.findIndex(item => item.id === id);
       if (index < 0) {
         throw new Error(`Item with id ${id} not found`);
       }
-      this.data.splice(index, 1);
+      this.placeData.splice(index, 1);
     }
   
     // 방어적 복사, string이 element니까 여기까지만 해도 문제 없다.
     findOne(id) {
-        const item = this.data.find(item => item.id === id);
+        const item = this.placeData.find(item => item.id === id);
         return item ? {...item} : null; // 객체의 복사본 반환
     }
   
     findAll() {
-        return this.data.map(item => ({...item})); // 배열의 요소들을 객체의 복사본으로 변환하여 반환
+        return this.placeData.map(item => ({...item})); // 배열의 요소들을 객체의 복사본으로 변환하여 반환
     }
 }
   
